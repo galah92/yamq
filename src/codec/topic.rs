3729cb60569eb99d7b5d189d95af12c6ff34046c
galah92 <galaharoni92@gmail.com>
2023-03-11 20:32:53 -0500

    Removed unused code in codec

--- src/codec/topic.rs
+++ src/codec/topic.rs
@@ -2,10 +2,7 @@ use std::str::FromStr;
 
 const TOPIC_SEPARATOR: char = '/';
 const MULTI_LEVEL_WILDCARD: char = '#';
-const MULTI_LEVEL_WILDCARD_STR: &str = "#";
 const SINGLE_LEVEL_WILDCARD: char = '+';
-const SINGLE_LEVEL_WILDCARD_STR: &str = "+";
-const SHARED_SUBSCRIPTION_PREFIX: &str = "$share/";
 const MAX_TOPIC_LEN_BYTES: usize = 65_535;
 
 /// A filter for subscribers to indicate which topics they want
@@ -15,8 +12,6 @@ const MAX_TOPIC_LEN_BYTES: usize = 65_535;
 pub enum TopicFilter {
     Concrete,
     Wildcard,
-    SharedConcrete,
-    SharedWildcard,
 }
 
 /// A topic name publishers use when sending MQTT messages.
@@ -24,7 +19,6 @@ pub enum TopicFilter {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Topic {
     topic_name: String,
-    level_count: u32,
 }
 
 impl Topic {
@@ -34,20 +28,11 @@ impl Topic {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum TopicLevel<'a> {
-    Concrete(&'a str),
-    SingleLevelWildcard,
-    MultiLevelWildcard,
-}
-
-#[derive(Debug, PartialEq, Eq)]
 pub enum TopicParseError {
     EmptyTopic,
     TopicTooLong,
     MultilevelWildcardNotAtEnd,
     InvalidWildcardLevel,
-    InvalidSharedGroupName,
-    EmptySharedGroupName,
     WildcardOrNullInTopic,
 }
 
@@ -55,28 +40,6 @@ impl std::fmt::Display for Topic {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.topic_name)
     }
-}
-
-/// If Ok, returns (level_count, contains_wildcards).
-fn process_filter(filter: &str) -> Result<(u32, bool), TopicParseError> {
-    let mut level_count = 0;
-    let mut contains_wildcards = false;
-    for level in filter.split(TOPIC_SEPARATOR) {
-        let level_contains_wildcard =
-            level.contains(|x: char| x == SINGLE_LEVEL_WILDCARD || x == MULTI_LEVEL_WILDCARD);
-        if level_contains_wildcard {
-            // Any wildcards on a particular level must be specified on their own
-            if level.len() > 1 {
-                return Err(TopicParseError::InvalidWildcardLevel);
-            }
-
-            contains_wildcards = true;
-        }
-
-        level_count += 1;
-    }
-
-    Ok((level_count, contains_wildcards))
 }
 
 impl FromStr for TopicFilter {
@@ -105,59 +68,25 @@ impl FromStr for TopicFilter {
             }
         }
 
-        let mut shared_group = None;
-
-        if let Some(filter_rest) = filter.strip_prefix(SHARED_SUBSCRIPTION_PREFIX) {
-            if filter_rest.is_empty() {
-                return Err(TopicParseError::EmptySharedGroupName);
-            }
-
-            if let Some(slash_pos) = filter_rest.find(TOPIC_SEPARATOR) {
-                let shared_name = &filter_rest[0..slash_pos];
-
-                // slash_pos+1 is safe here, we've already validated the string
-                // has a nonzero length.
-                let shared_filter = &filter_rest[(slash_pos + 1)..];
-
-                if shared_name.is_empty() {
-                    return Err(TopicParseError::EmptySharedGroupName);
+        let mut contains_wildcards = false;
+        for level in filter.split(TOPIC_SEPARATOR) {
+            let level_contains_wildcard =
+                level.contains(|x: char| x == SINGLE_LEVEL_WILDCARD || x == MULTI_LEVEL_WILDCARD);
+            if level_contains_wildcard {
+                // Any wildcards on a particular level must be specified on their own
+                if level.len() > 1 {
+                    return Err(TopicParseError::InvalidWildcardLevel);
                 }
 
-                if shared_name
-                    .contains(|x: char| x == SINGLE_LEVEL_WILDCARD || x == MULTI_LEVEL_WILDCARD)
-                {
-                    return Err(TopicParseError::InvalidSharedGroupName);
-                }
-
-                if shared_filter.is_empty() {
-                    return Err(TopicParseError::EmptyTopic);
-                }
-
-                shared_group = Some((shared_name, shared_filter))
-            } else {
-                return Err(TopicParseError::EmptyTopic);
+                contains_wildcards = true;
             }
         }
 
-        let topic_filter = if let Some((_, shared_filter)) = shared_group {
-            let (_, contains_wildcards) = process_filter(shared_filter)?;
-
-            if contains_wildcards {
-                TopicFilter::SharedWildcard
-            } else {
-                TopicFilter::SharedConcrete
-            }
+        if contains_wildcards {
+            Ok(TopicFilter::Wildcard)
         } else {
-            let (_, contains_wildcards) = process_filter(filter)?;
-
-            if contains_wildcards {
-                TopicFilter::Wildcard
-            } else {
-                TopicFilter::Concrete
-            }
-        };
-
-        Ok(topic_filter)
+            Ok(TopicFilter::Concrete)
+        }
     }
 }
 
@@ -186,31 +115,9 @@ impl FromStr for Topic {
             return Err(TopicParseError::WildcardOrNullInTopic);
         }
 
-        let level_count = topic.split(TOPIC_SEPARATOR).count() as u32;
-
-        let topic = Topic {
+        Ok(Topic {
             topic_name: topic.to_string(),
-            level_count,
-        };
-
-        Ok(topic)
-    }
-}
-
-pub struct TopicLevels<'a> {
-    levels_iter: std::str::Split<'a, char>,
-}
-
-impl<'a> Iterator for TopicLevels<'a> {
-    type Item = TopicLevel<'a>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.levels_iter.next() {
-            Some(MULTI_LEVEL_WILDCARD_STR) => Some(TopicLevel::MultiLevelWildcard),
-            Some(SINGLE_LEVEL_WILDCARD_STR) => Some(TopicLevel::SingleLevelWildcard),
-            Some(level) => Some(TopicLevel::Concrete(level)),
-            None => None,
-        }
+        })
     }
 }
 
@@ -300,107 +207,6 @@ mod tests {
     }
 
     #[test]
-    fn test_topic_filter_parse_shared_subscription_concrete() {
-        assert_eq!(
-            "$share/group_a/home".parse::<TopicFilter>().unwrap(),
-            TopicFilter::SharedConcrete
-        );
-
-        assert_eq!(
-            "$share/group_a/home/kitchen/temperature"
-                .parse::<TopicFilter>()
-                .unwrap(),
-            TopicFilter::SharedConcrete
-        );
-
-        assert_eq!(
-            "$share/group_a//".parse::<TopicFilter>().unwrap(),
-            TopicFilter::SharedConcrete
-        );
-    }
-
-    #[test]
-    fn test_topic_filter_parse_shared_subscription_wildcard() {
-        assert_eq!(
-            "$share/group_b/#".parse::<TopicFilter>().unwrap(),
-            TopicFilter::SharedWildcard
-        );
-
-        assert_eq!(
-            "$share/group_b/+".parse::<TopicFilter>().unwrap(),
-            TopicFilter::SharedWildcard
-        );
-
-        assert_eq!(
-            "$share/group_b/+/temperature"
-                .parse::<TopicFilter>()
-                .unwrap(),
-            TopicFilter::SharedWildcard
-        );
-
-        assert_eq!(
-            "$share/group_c/+/temperature/+/meta"
-                .parse::<TopicFilter>()
-                .unwrap(),
-            TopicFilter::SharedWildcard
-        );
-    }
-
-    #[test]
-    fn test_topic_filter_parse_invalid_shared_subscription() {
-        assert_eq!(
-            "$share/".parse::<TopicFilter>().unwrap_err(),
-            TopicParseError::EmptySharedGroupName
-        );
-        assert_eq!(
-            "$share/a".parse::<TopicFilter>().unwrap_err(),
-            TopicParseError::EmptyTopic
-        );
-        assert_eq!(
-            "$share/a/".parse::<TopicFilter>().unwrap_err(),
-            TopicParseError::EmptyTopic
-        );
-        assert_eq!(
-            "$share//".parse::<TopicFilter>().unwrap_err(),
-            TopicParseError::EmptySharedGroupName
-        );
-        assert_eq!(
-            "$share///".parse::<TopicFilter>().unwrap_err(),
-            TopicParseError::EmptySharedGroupName
-        );
-
-        assert_eq!(
-            "$share/invalid_group#/#"
-                .parse::<TopicFilter>()
-                .unwrap_err(),
-            TopicParseError::InvalidSharedGroupName
-        );
-    }
-
-    #[test]
-    fn test_topic_filter_parse_sys_prefix() {
-        assert_eq!(
-            "$SYS/stats".parse::<TopicFilter>().unwrap(),
-            TopicFilter::Concrete
-        );
-
-        assert_eq!(
-            "/$SYS/stats".parse::<TopicFilter>().unwrap(),
-            TopicFilter::Concrete
-        );
-
-        assert_eq!(
-            "$SYS/+".parse::<TopicFilter>().unwrap(),
-            TopicFilter::Wildcard
-        );
-
-        assert_eq!(
-            "$SYS/#".parse::<TopicFilter>().unwrap(),
-            TopicFilter::Wildcard
-        );
-    }
-
-    #[test]
     fn test_topic_filter_parse_invalid_filters() {
         assert_eq!(
             "sport/#/stats".parse::<TopicFilter>().unwrap_err(),
@@ -430,7 +236,6 @@ mod tests {
             "/".parse::<Topic>().unwrap(),
             Topic {
                 topic_name: "/".to_string(),
-                level_count: 2
             }
         );
 
@@ -438,7 +243,6 @@ mod tests {
             "Accounts payable".parse::<Topic>().unwrap(),
             Topic {
                 topic_name: "Accounts payable".to_string(),
-                level_count: 1
             }
         );
 
@@ -446,7 +250,6 @@ mod tests {
             "home/kitchen".parse::<Topic>().unwrap(),
             Topic {
                 topic_name: "home/kitchen".to_string(),
-                level_count: 2
             }
         );
 
@@ -454,7 +257,6 @@ mod tests {
             "home/kitchen/temperature".parse::<Topic>().unwrap(),
             Topic {
                 topic_name: "home/kitchen/temperature".to_string(),
-                level_count: 3
             }
         );
     }
